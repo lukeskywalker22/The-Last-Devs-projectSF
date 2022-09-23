1016e646854a7368b59a7fa1ea8b57667a6fb41a
lukeskywalker22 <ekulyeo22@gmail.com>
2022-09-23 11:53:59 +0800

    fixed referencing errors

---
+++ Swift/CodeComments/Messenger/Views/Cells/EventCell.swift
@@ -0,0 +1,43 @@
+//
+//  EventCell.swift
+//  EKEventKit.Example
+//
+//  Created by Filip Němeček on 30/03/2020.
+//  Copyright © 2020 Filip Němeček. All rights reserved.
+//
+
+import UIKit
+import EventKit
+
+class EventCell: UITableViewCell {
+    @IBOutlet var calendarColorView: UIView!
+    @IBOutlet var eventTitleLabel: UILabel!
+    @IBOutlet var eventDateLabel: UILabel!
+    @IBOutlet var eventDurationLabel: UILabel!
+    @IBOutlet var mapIcon: UIImageView!
+    
+    private static var relativeDateFormatter: RelativeDateTimeFormatter = {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.dateTimeStyle = .named
+        return formatter
+    }()
+    
+    private static var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
+    func configure(with event: EKEvent) {
+        eventTitleLabel.text = event.title
+        calendarColorView.backgroundColor = event.color
+        eventDurationLabel.text = event.isAllDay ? "all day" : formatDate(forNonAllDayEvent: event)
+        eventDateLabel.text = EventCell.relativeDateFormatter.localizedString(for: event.startDate, relativeTo: Date()).uppercased()
+        mapIcon.isHidden = !event.hasGeoLocation
+    }
+    
+    private func formatDate(forNonAllDayEvent event: EKEvent) -> String {
+        return "\(EventCell.dateFormatter.string(from: event.startDate)) - \(EventCell.dateFormatter.string(from: event.endDate))"
+    }
+
+}
